7a4e906c4a93855361efe2a43a9a24e1e4e9c43b
David <davcoding1@gmail.com>
2022-01-08 10:53:27 -0600

    Added comments

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onunload = () => window.scrollTo(0,0); // Go to top of window when page refreshed
 
-// DOTTED LINES //
+//// DOTTED LINES ////
 
 const drawDottedLines = () => {
     const docHeight = document.querySelector('main').getBoundingClientRect().height;
@@ -17,21 +17,22 @@ const drawDottedLines = () => {
         newLine.classList.remove('original'); 
         originalLine.after(newLine); //Inserts new line element after original
 
-        //Retrieves the middle position of both circles
+        //Retrieves the x and y values of the center points within both circles
         const x1 = circle1.offsetLeft + (circle1.offsetWidth / 2);
         const y1 = circle1.offsetTop + (circle1.offsetHeight / 2);
         const x2 = circle2.offsetLeft + (circle2.offsetWidth / 2);
         const y2 = circle2.offsetTop + (circle2.offsetHeight / 2);
 
-        //Connects both circles with a line element
+        //Creates a line element connecting the center points of both circles
         newLine.setAttribute('x1', x1); 
         newLine.setAttribute('y1', y1); 
         newLine.setAttribute('x2', x2); 
         newLine.setAttribute('y2', y2);
-    }
+    }; 
 
     document.querySelectorAll('.temporary').forEach(el => el.remove()); //Removes all previously drawn lines
 
+    //Runs the addDottedLine function for each circle element up to the second last one
     targetNodes.forEach((el, i) => { 
         if (i + 1 <= targetNodes.length - 1) addDottedLine(el, targetNodes[i + 1]); 
     }); 
@@ -42,62 +43,60 @@ const drawDottedLines = () => {
     window.addEventListener('resize', drawDottedLines); 
 })(); 
 
-// ELEMENT TRANSLATE //
+//// ELEMENT TRANSLATE ////
 
 (function translateElements() {
-    const initialElem = document.querySelectorAll('.initial-slide');
+    const initialElements = document.querySelectorAll('.initial-slide');
 
     const revealElement = (entries, observer) => {
         const [entry] = entries;
         
-        entry.target.classList.remove('initial-hidden');
+        entry.target.classList.remove('initial-hidden'); //Removes the inital-hidden class, causing the elements to translate upwards
         observer.unobserve(entry.target);
-        drawDottedLines(); 
-    }
+        drawDottedLines(); //Rerun the drawDottedLines function due to the repositioning of elements
+    }; 
 
-    const sectionObserver = new IntersectionObserver(revealElement, { threshold: [0, 0.1] });
+    const sectionObserver = new IntersectionObserver(revealElement, { threshold: [0, 0.1] }); 
 
-    initialElem.forEach((element) => {
+    initialElements.forEach((element) => {
         sectionObserver.observe(element);
         element.classList.add('initial-hidden');
     });
 })(); 
 
 
-// PERFORMANCE SLIDER //
+//// PERFORMANCE SLIDER ////
 
 (function initializeSlider() {
     const slides = document.querySelectorAll('.performance');
-    const btnLeft = document.querySelector('.button-left');
-    const btnRight = document.querySelector('.button-right');
+    const buttonLeft = document.querySelector('.button-left');
+    const buttonRight = document.querySelector('.button-right');
 
     let currentSlide = 0;
     const maxSlide = slides.length;
 
     const goToSlide = (slide) => {
-        slides.forEach(
-            (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-        );
-    }
+        slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    }; 
 
     const handleNextSlide = () => {
         if (currentSlide === maxSlide - 1) currentSlide = 0;
         else currentSlide++;
 
         goToSlide(currentSlide);
-    }
+    }; 
 
     const handlePrevSlide = () => {
         if (currentSlide === 0) currentSlide = maxSlide - 1;
         else currentSlide--;
 
         goToSlide(currentSlide);
-    }
+    }; 
 
     goToSlide(0);
 
-    btnRight.addEventListener('click', handleNextSlide);
-    btnLeft.addEventListener('click', handlePrevSlide);
+    buttonRight.addEventListener('click', handleNextSlide);
+    buttonLeft.addEventListener('click', handlePrevSlide);
 
     document.addEventListener('keydown', (e) => {
         if (e.key === 'ArrowRight') handleNextSlide();
@@ -125,13 +124,13 @@ const drawDottedLines = () => {
                 audioElements[i].currentTime = 0; 
             }); 
         }); 
-    }
+    }; 
 
-    audioCheckElement.style.transform = 'translateX(0px)'; 
+    audioCheckElement.style.transform = 'translateX(0px)'; //Shows the audioCheckElement
 
     audioCheckElement.addEventListener('click', (e) => {
         e.preventDefault(); 
-        audioCheckElement.style.transform = 'translateX(-500px)'; 
+        audioCheckElement.style.transform = 'translateX(-500px)'; //Hides the audioCheckElement
         handleAudio(); 
     }); 
 })(); 
