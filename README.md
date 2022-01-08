73ce7dc53a9fd699e7547525de0866e3cb8fcb10
DavidB09 <davcoding1@gmail.com>
2022-01-07 20:11:48 -0600

    Added comments to JS file and README

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 A repo for a tribute page of Son House created by David Brunner 
 
 [Link](https://son-house-tribute-page.netlify.app)
+
+## Skills Learned 
+- Responsive web design
+- CSS grid positioning
+- Altering SVG elements based on window resizing
+- Tranformations and transitions within CSS
+- Audio manipulation based on user input 
--- script.js
+++ script.js
@@ -15,20 +15,22 @@ const drawDottedLines = () => {
         const newLine = originalLine.cloneNode(false); 
         newLine.classList.add('temporary'); 
         newLine.classList.remove('original'); 
-        originalLine.after(newLine); 
+        originalLine.after(newLine); //Inserts new line element after original
 
+        //Retrieves the middle position of both circles
         const x1 = circle1.offsetLeft + (circle1.offsetWidth / 2);
         const y1 = circle1.offsetTop + (circle1.offsetHeight / 2);
         const x2 = circle2.offsetLeft + (circle2.offsetWidth / 2);
         const y2 = circle2.offsetTop + (circle2.offsetHeight / 2);
 
+        //Connects both circles with a line element
         newLine.setAttribute('x1', x1); 
         newLine.setAttribute('y1', y1); 
         newLine.setAttribute('x2', x2); 
         newLine.setAttribute('y2', y2);
     }
 
-    document.querySelectorAll('.temporary').forEach(el => el.remove()); 
+    document.querySelectorAll('.temporary').forEach(el => el.remove()); //Removes all previously drawn lines
 
     targetNodes.forEach((el, i) => { 
         if (i + 1 <= targetNodes.length - 1) addDottedLine(el, targetNodes[i + 1]); 
@@ -59,7 +61,6 @@ const drawDottedLines = () => {
         sectionObserver.observe(element);
         element.classList.add('initial-hidden');
     });
-
 })(); 
 
 
@@ -102,14 +103,12 @@ const drawDottedLines = () => {
         if (e.key === 'ArrowRight') handleNextSlide();
         if (e.key === 'ArrowLeft') handlePrevSlide();
     });
-
 })();
 
 
 //// AUDIO ////
 
 (function initializeAudio() {
-
     const audioCheckElement = document.querySelector('.audio-check'); 
 
     const handleAudio = () => {
@@ -135,5 +134,4 @@ const drawDottedLines = () => {
         audioCheckElement.style.transform = 'translateX(-500px)'; 
         handleAudio(); 
     }); 
-
 })(); 
